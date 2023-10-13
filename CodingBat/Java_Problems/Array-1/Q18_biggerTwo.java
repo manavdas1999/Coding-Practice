402085d59245e2f6639b816dd5c9d6e5e2a2dea9
manavdas1999 <dascorp009@gmail.com>
2023-10-13 18:29:20 +0530

    Added: Array-1-Q18-20

---
+++ CodingBat/Java_Problems/Array-1/Q18_biggerTwo.java
@@ -0,0 +1,18 @@
+
+/*
+
+Start with 2 int arrays, a and b, each length 2. Consider the sum of the values in each array. Return the array which has the largest sum. In event of a tie, return a.
+
+
+biggerTwo([1, 2], [3, 4]) → [3, 4]
+biggerTwo([3, 4], [1, 2]) → [3, 4]
+biggerTwo([1, 1], [1, 2]) → [1, 2]
+*/
+
+public class Q18_biggerTwo {
+    public int[] biggerTwo(int[] a, int[] b) {
+        int aSum = a[0] + a[1];
+        int bSum = b[0] + b[1];
+        return aSum >= bSum ? a : b;
+    }
+}
