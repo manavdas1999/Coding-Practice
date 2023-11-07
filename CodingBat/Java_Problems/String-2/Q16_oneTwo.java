9e8a26c93e4ec61a34a70d6811acf9564e60a266
manavdas1999 <dascorp009@gmail.com>
2023-11-07 20:39:27 +0530

    Added: String-2-Q16-17

---
+++ CodingBat/Java_Problems/String-2/Q16_oneTwo.java
@@ -0,0 +1,20 @@
+
+/* 
+
+Given a string, compute a new string by moving the first char to come after the next two chars, so "abc" yields "bca". Repeat this process for each subsequent group of 3 chars, so "abcdef" yields "bcaefd". Ignore any group of fewer than 3 chars at the end.
+
+
+oneTwo("abc") → "bca"
+oneTwo("tca") → "cat"
+oneTwo("tcagdo") → "catdog"
+*/
+
+public class Q16_oneTwo {
+    public String oneTwo(String str) {
+        String res = "";
+        for(int i=0; i<str.length()-2; i+=3){
+          res += str.substring(i+1, i+3) + str.charAt(i);
+        }
+        return res;
+      }
+}
---
+++ CodingBat/Java_Problems/String-2/Q17_zipZap.java
@@ -0,0 +1,29 @@
+
+/* 
+
+Look for patterns like "zip" and "zap" in the string -- length-3, starting with 'z' and ending with 'p'. Return a string where for all such words, the middle letter is gone, so "zipXzap" yields "zpXzp".
+
+
+zipZap("zipXzap") → "zpXzp"
+zipZap("zopzop") → "zpzp"
+zipZap("zzzopzop") → "zzzpzp"
+*/
+
+public class Q17_zipZap {
+    public String zipZap(String str) {
+        String res = "";
+        int i = 0;
+        if(str.length() < 3) return str;
+        
+        while(i < str.length()){
+          res += str.charAt(i);
+          if(str.charAt(i) == 'z' && str.charAt(i+2) == 'p'){
+            i+=2;
+          }
+          else{
+            i++;
+          }
+        }
+        return res;
+      }
+}
