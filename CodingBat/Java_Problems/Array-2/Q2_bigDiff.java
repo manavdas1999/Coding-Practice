3ca3a6b5c97fd21fe4eece7530decc25db6fb6c5
manavdas1999 <dascorp009@gmail.com>
2023-11-08 21:47:30 +0530

    Added: Array-2-Q1-2

---
+++ CodingBat/Java_Problems/Array-2/Q2_bigDiff.java
@@ -0,0 +1,23 @@
+
+/*
+
+Given an array length 1 or more of ints, return the difference between the largest and smallest values in the array. Note: the built-in Math.min(v1, v2) and Math.max(v1, v2) methods return the smaller or larger of two values.
+
+
+bigDiff([10, 3, 5, 6]) → 7
+bigDiff([7, 2, 10, 9]) → 8
+bigDiff([2, 10, 7, 2]) → 8
+ */
+
+public class Q2_bigDiff {
+    public int bigDiff(int[] nums) {
+        // assuming no negative value
+        int max = nums[0];
+        int min = nums[0];
+        for(int i=1; i<nums.length; i++){
+          max = Math.max(max, nums[i]);
+          min = Math.min(min, nums[i]);
+        }
+        return max-min;
+      }
+}
