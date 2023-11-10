22f66506ca14764eadd6d047414272962ae56c08
manavdas1999 <dascorp009@gmail.com>
2023-11-10 20:15:35 +0530

    Added: Array-2-Q13-14

---
+++ CodingBat/Java_Problems/Array-2/Q13_either24.java
@@ -0,0 +1,22 @@
+
+/*
+Given an array of ints, return true if the array contains a 2 next to a 2 or a 4 next to a 4, but not both.
+
+
+either24([1, 2, 2]) → true
+either24([4, 4, 1]) → true
+either24([4, 4, 1, 2, 2]) → false
+ */
+
+public class Q13_either24 {
+    public boolean either24(int[] nums) {
+        int twoPair = 0;
+        int fourPair = 0;
+        for(int i=0; i<nums.length-1; i++){
+          if(nums[i] == 2 && nums[i+1] == 2) twoPair++;
+          if(nums[i] == 4 && nums[i+1] == 4) fourPair++;
+        }
+        return (twoPair>0 && fourPair == 0) || (fourPair>0 && twoPair == 0);
+      }
+      
+}
